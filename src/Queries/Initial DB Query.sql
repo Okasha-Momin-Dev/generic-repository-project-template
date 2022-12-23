43235117b37c42d79c7214e4e0d429bcf05dfa01
Okasha Momin <okashamomin.softprodigy@gmail.com>
2022-12-23 10:53:23 +0530

    db query updated

--- src/Queries/Initial DB Query.sql
+++ src/Queries/Initial DB Query.sql
@@ -24,12 +24,11 @@ CREATE TABLE dbo.Department		(	Id			INT IDENTITY PRIMARY KEY,
 									UpdatedBy	INT NOT NULL,
 									UpdatedOn	DATETIME NOT NULL)
 
-
 CREATE TABLE dbo.Employee		(	Id			INT	IDENTITY PRIMARY KEY,
 									EmpName		VARCHAR(100) NOT NULL,
 									EmpCode		VARCHAR(15) NOT NULL,
 									Designation	VARCHAR(50) NOT NULL,
-									Salary		DECIMAL(18,2) NOT NULL,
+									Salary		DECIMAL(18,2) NULL,
 									ManagerId	INT NOT NULL,
 									DeptId		INT NOT NULL,
 									IsActive	BIT NOT NULL,
@@ -38,7 +37,8 @@ CREATE TABLE dbo.Employee		(	Id			INT	IDENTITY PRIMARY KEY,
 									UpdatedBy	INT NOT NULL,
 									UpdatedOn	DATETIME NOT NULL,
 									CONSTRAINT  FK_Employee_Department FOREIGN KEY (DeptId) REFERENCES Department(Id),
-									CONSTRAINT  FK_Employee_ApplicationUser FOREIGN KEY (Id) REFERENCES ApplicationUser(Id))
+									CONSTRAINT  FK_Employee_ApplicationUser FOREIGN KEY (Id) REFERENCES ApplicationUser(Id),
+									CONSTRAINT  FK_Employee_EmpManager FOREIGN KEY (ManagerId) REFERENCES Employee(Id))
 
 
 
